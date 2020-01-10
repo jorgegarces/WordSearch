c856fe056ad1323ce01b19dfff2abc90255468f1
Jorge Garcés <jgarcesembarba@gmail.com>
2020-01-10 12:10:52 +0100

    feature: works with multiple rows and one word horizontally

--- src/main/java/WordSearch.java
+++ src/main/java/WordSearch.java
@@ -1,20 +1,57 @@
+import java.util.Arrays;
+
 import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class WordSearch {
-    private final String line1;
+    private final String[] board;
     private final String word1;
+    private final int rows;
 
-    public WordSearch(String line1, String word1) {
-        this.line1 = line1;
+    public WordSearch(String[] board, String word1, int rows) {
+        this.board = board;
         this.word1 = word1;
+        this.rows = rows;
     }
 
-    public String search() {
-        String result = new String();
-        for(int i = 0; i < line1.length(); i++) {
-            if (line1.charAt(i) != word1.charAt(0)) result += line1.charAt(i);
-            else result += toUpperCase(line1.charAt(i));
+    public String[] run() {
+        for (int i = 0; i < rows; i++) {
+            board[i] = match(board[i]);
         }
-        return result;
+        return board;
+}
+
+    private String match(String line) {
+        StringBuilder result = new StringBuilder("");
+        StringBuilder helper = new StringBuilder("");
+
+        int i = 0;
+        int j = 0;
+
+        while (i < line.length()) {
+            if (line.charAt(i) != word1.charAt(j)) {
+                result.append(line.charAt(i));
+                i++;
+            }
+
+            else {
+                if (helper.length() == 0) helper.append(toUpperCase(line.charAt(i)));
+                else if (helper.charAt(helper.length() -1) != line.charAt(i)) {
+                    helper.append(toUpperCase(line.charAt(i)));
+
+                }
+                if (helper.length() == word1.length()) result.append(helper);
+                i++;
+                if (j < word1.length() - 1) j++;
+            }
+        }
+        return result.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WordSearch that = (WordSearch) o;
+        return Arrays.equals(board, that.board);
     }
 }
