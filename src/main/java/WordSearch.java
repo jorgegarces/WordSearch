47ea7afdf455d0fcc16af914875acbc62727de82
Jorge Garcés <jgarcesembarba@gmail.com>
2020-01-13 12:19:22 +0100

    refactor: total rehaul from start

--- src/main/java/WordSearch.java
+++ src/main/java/WordSearch.java
@@ -1,57 +1,5 @@
-import java.util.Arrays;
 
-import static jdk.nashorn.internal.objects.NativeString.toUpperCase;
 
 public class WordSearch {
-    private final String[] board;
-    private final String word1;
-    private final int rows;
 
-    public WordSearch(String[] board, String word1, int rows) {
-        this.board = board;
-        this.word1 = word1;
-        this.rows = rows;
-    }
-
-    public String[] run() {
-        for (int i = 0; i < rows; i++) {
-            board[i] = match(board[i]);
-        }
-        return board;
-}
-
-    private String match(String line) {
-        StringBuilder result = new StringBuilder("");
-        StringBuilder helper = new StringBuilder("");
-
-        int i = 0;
-        int j = 0;
-
-        while (i < line.length()) {
-            if (line.charAt(i) != word1.charAt(j)) {
-                result.append(line.charAt(i));
-                i++;
-            }
-
-            else {
-                if (helper.length() == 0) helper.append(toUpperCase(line.charAt(i)));
-                else if (helper.charAt(helper.length() -1) != line.charAt(i)) {
-                    helper.append(toUpperCase(line.charAt(i)));
-
-                }
-                if (helper.length() == word1.length()) result.append(helper);
-                i++;
-                if (j < word1.length() - 1) j++;
-            }
-        }
-        return result.toString();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        WordSearch that = (WordSearch) o;
-        return Arrays.equals(board, that.board);
-    }
 }
--- src/test/java/WordSearchShould.java
+++ src/test/java/WordSearchShould.java
@@ -2,66 +2,12 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class WordSearchShould {
-    @Test
-    public void work_with_one_row_and_one_letter_horizontally() {
-        int rows = 1;
-        int columns = 6;
-        int words = 1;
-
-        String word1 = "g";
-
-        String line1 = "zdefgz";
-        String[] board = new String[] {line1};
-
-        String expected1 = "zdefGz";
-        String[] expected = new String[] {expected1};
-
-        WordSearch wordSearch = new WordSearch(board, word1, rows);
-        Assert.assertArrayEquals(expected, wordSearch.run());
-        System.out.println(expected.toString());
-        System.out.println(wordSearch.run().toString());
-    }
-
-    @Test
-    public void work_with_one_row_and_a_three_letter_word_horizontally() {
-        int rows = 1;
-        int columns = 6;
-        int words = 1;
-
-        String word1 = "def";
-
-        String line1 = "zdefgz";
-        String[] board = new String[] {line1};
-
-        String expected1 = "zDEFgz";
-        String[] expected = new String[] {expected1};
-
-        WordSearch wordSearch = new WordSearch(board, word1, rows);
-        Assert.assertArrayEquals(expected, wordSearch.run());
-    }
 
     @Test
     public void work_with_multiple_rows_and_one_word_horizontally(){
-        int rows = 3;
-        int columns = 6;
-        int words = 1;
 
-        String word1 = "def";
 
-        String line1 = "zzzzzz";
-        String line2 = "zdefgz";
-        String line3 = "zzzzzz";
-
-        String[] board = new String[] {line1, line2, line3};
-
-        String expected1 = "zzzzzz";
-        String expected2 = "zDEFgz";
-        String expected3 = "zzzzzz";
-
-        String[] expected = new String[] {expected1, expected2, expected3};
-
-        WordSearch wordSearch = new WordSearch(board, word1, rows);
-
-    Assert.assertArrayEquals(expected, wordSearch.run());
+        Assert.assertArrayEquals(expected, wordSearch.run());
     }
+
 }
