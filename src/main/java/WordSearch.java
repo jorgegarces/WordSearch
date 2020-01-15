49480b6250a44af4771b0694d7c19d0d1360b292
Jorge Garcés <jgarcesembarba@gmail.com>
2020-01-15 11:26:23 +0100

    feature: diagonalLeftToRightDown and horizontalLefttoRight unilimited matrix

--- src/main/java/WordSearch.java
+++ src/main/java/WordSearch.java
@@ -1,5 +1,73 @@
-
-
 public class WordSearch {
+
+    private final String[] wordArray;
+    private char[][] board;
+
+    public WordSearch(String[] wordArray, char[][] board) {
+        this.wordArray = wordArray;
+        this.board = board;
+    }
+
+    public char[][] wordSearch() {
+
+        for (String currentWord : wordArray) {
+            board = traverseMatrix(currentWord);
+        }
+
+        return board;
+    }
+
+    private char[][] traverseMatrix(String currentWord) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == currentWord.charAt(0)) {
+                    diagonalLeftToRightDown(currentWord, i, j);
+                    horizontalLeftToRight(currentWord, i, j);
+                }
+            }
+        }
+
+        return board;
+    }
+
+    private void diagonalLeftToRightDown(String currentWord, int i, int j) {
+        int counter = 0;
+        for (int k = i, l = j; counter < currentWord.length(); k++, l++) {
+            if (board[k][l] != currentWord.charAt(counter)) break;
+            if (board[k][l] == currentWord.charAt(counter)) counter++;
+            if (l == board[k].length -1) l = -1;
+            if (k == board.length -1) k = -1;
+
+
+        }
+        if (counter == currentWord.length()) {
+            counter = 0;
+            for (int k = i, l = j; counter < currentWord.length(); k++, l++, counter++) {
+                board[k][l] = Character.toUpperCase(board[k][l]);
+                if (l == board[k].length -1) l = -1;
+                if (k == board.length -1) k = -1;
+            }
+        }
+    }
+
+    private void horizontalLeftToRight(String currentWord, int i, int j) {
+        int counter = 0;
+        for (int k = i, l = j; counter < currentWord.length(); l++) {
+            if (board[k][l] != currentWord.charAt(counter)) break;
+            if (board[k][l] == currentWord.charAt(counter)) counter++;
+            if (l == board[k].length -1) l = -1;
+            if (k == board.length -1) k = -1;
+
+        }
+        if (counter == currentWord.length()) {
+            counter = 0;
+            for (int k = i, l = j; counter < currentWord.length(); l++, counter++) {
+                board[k][l] = Character.toUpperCase(board[k][l]);
+                if (l == board[k].length -1) l = -1;
+                if (k == board.length -1) k = -1;
+            }
+        }
+    }
 
 }
--- src/test/java/WordSearchShould.java
+++ src/test/java/WordSearchShould.java
@@ -4,10 +4,44 @@ import org.junit.Test;
 public class WordSearchShould {
 
     @Test
-    public void work_with_multiple_rows_and_one_word_horizontally(){
+    public void find_a_word_Diagonal_Left_to_Right_Down(){
+        String[] words = new String[] {"lbf", "dcc"};
 
+        char[][] board = new char[][] {
+                {'a', 'b', 'c'},
+                {'d', 'd', 'f'},
+                {'l', 'c', 'c'}
+        };
 
-        Assert.assertArrayEquals(expected, wordSearch.run());
+        char[][] expected = new char[][] {
+                {'a', 'B', 'C'},
+                {'D', 'd', 'F'},
+                {'L', 'C', 'c'}
+        };
+
+        WordSearch result = new WordSearch(words, board);
+
+        Assert.assertArrayEquals(expected, result.wordSearch());
     }
 
+    @Test
+    public void find_a_word_Horizontal_Left_to_Right(){
+        String[] words = new String[] {"bca", "df"};
+
+        char[][] board = new char[][] {
+                {'a', 'b', 'c'},
+                {'d', 'd', 'f'},
+                {'l', 'c', 'c'}
+        };
+
+        char[][] expected = new char[][] {
+                {'A', 'B', 'C'},
+                {'d', 'D', 'F'},
+                {'l', 'c', 'c'}
+        };
+
+        WordSearch result = new WordSearch(words, board);
+
+        Assert.assertArrayEquals(expected, result.wordSearch());
+    }
 }
